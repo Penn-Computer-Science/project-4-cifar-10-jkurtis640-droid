b1e515f3af484ba4897a9b3fb495f4043f6e1e0e
jkurtis640-droid <jkurtis640@phm.k12.in.us>
2025-11-19 09:55:09 -0500

    commit

--- notcifar10.py
+++ notcifar10.py
@@ -117,6 +117,7 @@ for layer_index, activation in enumerate(activations):
     
     plt.tight_layout()
     plt.show()
+    
 ## Reflection
 ## 1. I noticed that mnist used on hot and required RMSprop while Cifar 10 used  the adam optimizer and used to_categrical.
 ## I also noticed that MNist was predictions based on numbers 0-9 while Cifar 10 was predictions based on physical things like airplane frog horse etc.
