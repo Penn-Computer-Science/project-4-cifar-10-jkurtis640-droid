22f59b9bd6c01337de42d3735946eb4471ec133e
jkurtis640-droid <jkurtis640@phm.k12.in.us>
2025-12-12 09:51:45 -0500

    Update not_maze_ai_project.py

--- not_maze_ai_project.py
+++ not_maze_ai_project.py
@@ -1,4 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import time
 import turtle
+import random
 
 tile_size = 30
 ##this is a change
@@ -119,12 +123,97 @@ screen.onkey(go_right,"Right")
 
 turtle.done()
 
+actions = [(-1,0)
+           (1,0)
+           (0,-1)
+           (0,1)]
 
+class QLearningAgent:
+    def __init__(self, maze, learning_rate=0.1, discount_factor=0.9, exploration_start=1.0, exploration_end=0.01, num_episodes=100):
 
+        self.q_table = np.zeros((maze.maze_height, maze.maze_width, 4))
+        self.learning_rate = learning_rate
+        self.discount_factor = discount_factor
+        self.exploration_start = exploration_start
+        self.exploration_end = exploration_end
+        self.num_episodes = num_episodes
 
-
-
-
-
-
+    def get_exploration_rate(self, current_episode):
+        exploration_rate = self.exploration_start * (self.exploration_end / self.exploration_start) ** (current_episode / self.num_episodes)
+        return exploration_rate
     
+    def get_action(self, state, current_episode):
+        exploration_rate = self.get_exploration_rate(current_episode)
+        if np.random.rand() < exploration_rate:
+            return np.random.randint(4)
+        else:
+            return np.argmax(self.q_table[state])
+        
+    def update_q_table(self, state, action, next_state, reward):
+        best_next_action = np.argmax(self.q_table[next_state])
+
+        current_q_value = self.q_table[state][action]
+
+        new_q_value = current_q_value + self.learning_rate * (reward + self.discount_factor * self.q_table[next_state][best_next_action] - current_q_value)
+        return new_q_value
+
+goal_reward = 100
+wall_penalty = -10
+step_penalty = -1
+
+def finish_episode(agent, maze, current_episode, train=True):
+    current_state = maze.start_position
+    is_done = False
+    episode_reward = 0
+    episode_step = 0
+    path = [current_state]
+    
+    while not is_done:
+
+        action = agent.get_action(current_state,current_episode)
+
+        next_state = (current_state[0] + actions[action][0], current_state[1], actions[action][1])
+            
+        if next_state[0] < 0 or next_state[0] >= maze.maze_height or next_state[1] < 0 or next_state[1] >= maze.maze_width or maze.maze[next_state[1]][next_state[0]] == 1:
+            reward = wall_penalty
+            next_state = current_state
+
+        elif next_state == (maze.goal_position):
+             path.append(current_state)
+             reward = goal_reward
+             is_done = True
+
+        else:
+            path.append(current_state)
+            reward = step_penalty
+
+        episode_reward += reward
+        episode_step += 1
+
+        if train == True:
+            agent.update_q_table(current_state, action, next_state, reward)
+
+        current_state = next_state
+
+    return episode_reward, episode_step, path
+
+def test_agent(agent, maze, num_episodes=1):
+    
+    episode_reward, episode_step, path = finish_episode(agent, maze, num_episodes, train=False)
+
+    print("Learned Path")
+    for row, col in path:
+        print(f"({row}, {cols}->", end='')
+    print("Goal")
+
+    print("Number of steps:", episode_step)
+    print("Total Reward:", episode_reward)
+
+    if plt.gcf().get_axes():
+        plt.cla()
+
+    plt.figure(figsize=(5,5))
+    plt.imshow(maze.maze, cmap="gray")
+
+    plt.text(maze.start_position[0], maze.start_postion[1], 'S', ha='center', va='center',color='red', fontsize=20)
+    plt.text(maze.goal_position[0], maze.goal_postion[1], 'G', ha='center', va='center',color='green', fontsize=20)
